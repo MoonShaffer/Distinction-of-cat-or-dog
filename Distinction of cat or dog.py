6fd53603107baccfafa459362ea26e2afd524a97
MoonShaffer <entyulpzkbe@gmail.com>
2021-11-21 18:05:10 +0900

    reupload

--- Distinction of cat or dog.py
+++ Distinction of cat or dog.py
@@ -1,3 +1,28 @@
+# 必要なライブラリのインポート
+
+import numpy as np
+import pandas as pd
+from PIL import Image
+from sklearn.svm import SVC
+from sklearn import metrics
+import matplotlib.pyplot as plt
+
+%matplotlib inline
+
+# 訓練データ用CSVの読み込み
+
+train_data = pd.read_csv("train/train_data.csv")
+train_data.head()
+
+# テストデータ用CSVの読み込み
+
+test_data = pd.read_csv("test/test_data.csv")
+test_data.head()
+
+
+
+
+
 # ねこの写真をグレースケール化して表示
 
 sample_img1 = Image.open("train/cat-089.jpg").convert("L")
